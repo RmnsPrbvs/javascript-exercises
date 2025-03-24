3ba5471a2d97d171ae6baa08407f972b30704375
spam <61126728+RmnsPrbvs@users.noreply.github.com>
2025-03-24 04:34:12 -0700

    task 6 complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year / 4) && (!(Number.isInteger(year / 100)) || (Number.isInteger(year / 400)))) {
+        return true
+    } 
+    return false
 };
+
+console.log(leapYears(2000))
 
 // Do not edit below this line
 module.exports = leapYears;
