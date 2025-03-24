b3393678bf97df5f58e7abc2ec10184a62cce57c
spam <61126728+RmnsPrbvs@users.noreply.github.com>
2025-03-24 04:09:18 -0700

    task 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (a < 0 || b < 0) {return "ERROR"}
+        if (a > b) {
+            let c = a
+            a = b
+            b = c
+        }
+    
+    let m = 0
+    for (i = a; i < b; i++) {
+        m = m + i
+        console.log(m)
+    }
+    return m + b 
+    } else {
+        return "ERROR"
+    }
 
 };
+
+console.log(sumAll(123, -1))
 
 // Do not edit below this line
 module.exports = sumAll;
