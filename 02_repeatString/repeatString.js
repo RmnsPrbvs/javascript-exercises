f4a74bbe78d1862045e0cead27adeb38c928d397
spam <61126728+RmnsPrbvs@users.noreply.github.com>
2025-03-24 02:40:11 -0700

    task 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+    return "ERROR"
+    } else {
+    let st = ""
+    for (i = 0; i < num; i++) {
+        st = st + str
+    }
+    return st
+}
 };
 
 // Do not edit below this line
