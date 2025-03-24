4ebcec89197d161a6f456e669aa93b8e4a42df3d
spam <61126728+RmnsPrbvs@users.noreply.github.com>
2025-03-24 03:00:18 -0700

    task 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let zon = []
+    for (i in str) {
+        console.log(str[i])
+        zon.push(str[i])
+    }
+    zon.reverse()
+    mq = ""
+    for (i in zon) {
+        mq = mq + zon[i]
+    }
+    return mq
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
