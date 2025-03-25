62e94021229370fe244b77583f5afff3a68b6b44
spam <61126728+RmnsPrbvs@users.noreply.github.com>
2025-03-25 00:03:38 -0700

    task 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num = (num - 32) * 5/9
+  return Number(num.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num = (num * (9/5)) + 32
+  return Number(num.toFixed(1))
 };
 
+console.log(convertToCelsius(32))
+console.log(convertToCelsius(-100))
+console.log(convertToFahrenheit(73.2))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
